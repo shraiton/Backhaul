8ce2e7b6f794e91bc64c18327e14f35c18182a8b
gitea <gitea>
2024-12-25 21:10:14 +0330

    ii

--- internal/server/transport/bufferedconn.go
+++ internal/server/transport/bufferedconn.go
@@ -16,6 +16,7 @@ type BufferedConn struct {
 	buffer    *[]byte    // Buffer to store the initial 4KB
 	bufferPos int        // Current read position in the buffer
 	mu        sync.Mutex // Mutex to protect buffer access
+	closed    bool
 }
 
 // NewBufferedConn initializes a BufferedConn by reading the first 4KB.
@@ -28,6 +29,7 @@ func NewBufferedConn(conn net.Conn) (*BufferedConn, error) {
 		Conn:      conn,
 		buffer:    buffer,
 		bufferPos: 0,
+		closed:    false,
 	}
 
 	conn.SetReadDeadline(time.Now().Add(time.Duration(3 * time.Second)))
@@ -50,11 +52,10 @@ func NewBufferedConn(conn net.Conn) (*BufferedConn, error) {
 
 // Read overrides the default Read method to first serve data from the buffer.
 func (bc *BufferedConn) Read(p []byte) (int, error) {
-	//bc.mu.Lock()
-	//defer bc.mu.Unlock()
 
-	// If there is data in the buffer, read from it first
-	if bc.buffer != nil && bc.bufferPos < len(*bc.buffer) {
+	bc.mu.Lock()
+	defer bc.mu.Unlock()
+	if bc.buffer != nil && bc.closed == false && bc.bufferPos < len(*bc.buffer) { // if it was closed don't come here at all
 		n := copy(p, (*bc.buffer)[bc.bufferPos:])
 		bc.bufferPos += n
 
@@ -65,15 +66,20 @@ func (bc *BufferedConn) Read(p []byte) (int, error) {
 
 		return n, nil
 	}
+	//the first part will return and unlocks the mu for sure then when we return the bc.Conn.Read the mu is already unlocked
 
 	// Buffer has been fully read; delegate to the underlying connection
 	return bc.Conn.Read(p)
 }
 
 func (bc *BufferedConn) Close() error {
+	bc.mu.Lock()
 	if bc.buffer != nil {
 		utils.PutBuffer4k(bc.buffer) // Return buffer to the pool
 		bc.buffer = nil
 	}
+	bc.closed = true
+	bc.mu.Unlock()
+
 	return bc.Conn.Close()
 }
